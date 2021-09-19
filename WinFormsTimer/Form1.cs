beb9c5b4fedeac4b4eda568b4cabaac9bd9fcdc8
Koson Trachu <koson.trachu@gmail.com>
2021-09-20 00:37:14 +0700

    Add project files.

---
+++ WinFormsTimer/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace WinFormsTimer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Invoke( new MethodInvoker( delegate()
+            {
+                label1.Text = DateTime.Now.ToString("HH:mm:ss");
+            }));
+        }
+    }
+}
